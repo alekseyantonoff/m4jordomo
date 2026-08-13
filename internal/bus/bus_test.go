16235a33dfc36b444167bd334464031b2226ec4f
Aleksey Antonov <aleksey.antonoff@gmail.com>
2026-08-13 23:57:49 +0700

    bus_test2

--- internal/bus/bus_test.go
+++ internal/bus/bus_test.go
@@ -21,3 +21,26 @@ func TestSubscribeAndPublish(t *testing.T) {
 		t.Errorf("обработчик не вызван: received=%v", received)
 	}
 }
+
+func TestAllHandlersAreCalled(t *testing.T) {
+	count := 0
+	b := New()
+
+	b.Subscribe("test.fanout", func(_ types.Event) {
+		count++
+	})
+
+	b.Subscribe("test.fanout", func(_ types.Event) {
+		count++
+	})
+
+	b.Publish(types.Event{
+		Type:     "test.fanout",
+		Priority: types.Medium,
+	})
+
+	const want = 2
+	if count != want {
+		t.Errorf("вызвано обработчиков: got=%d, want=%d", count, want)
+	}
+}
